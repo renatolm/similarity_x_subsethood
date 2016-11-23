f9376a5f9179ff7eac20e65eebebaecbfd62d36b
renatolm <rlmoura@icloud.com>
2016-11-23 19:45:31 -0200

    working on proofs of propositions

--- numero.py
+++ numero.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import pylab as pl
 
@@ -10,16 +11,28 @@ def centralize(inp, universo):
 
 	return inpY
 
+def getNumberFromAlphaCut(universo):
+	a = np.zeros((len(universo),),float)
+	for alpha in np.arange(0,1,0.001):
+		pos = 0
+		for i in universo:
+			if (i>(2/5*alpha + 4/5)) and (i<(-4*alpha + 16)):
+				a[pos] = alpha
+			pos = pos+1
+
+	return a
 
 #Input
 inp = [2.0,3.0,4.0]
 
 inpY = []
 
-universo = np.arange(-10,10,0.001)
+universo = np.arange(0,20,0.001)
 
-inpY = centralize(inp, universo)
+#inpY = centralize(inp, universo)
+inpY = getNumberFromAlphaCut(universo)
 
 pl.plot(universo,inpY, 'b')
-pl.axis([-10, 10, 0, 1])
-pl.savefig("numero_center.png")
+pl.axis([0, 20, 0, 1.1])
+pl.savefig("numero_resultado.png")
+#pl.show()
